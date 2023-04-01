7443ab5f915cfad817500579e413f6a8e720eb8c
FanBBBBB <3190104613@zju.edu.cn>
2023-04-01 22:44:07 +0800

    mobx、axios、token待完成

--- gamescore/src/pages/Login/index.js
+++ gamescore/src/pages/Login/index.js
@@ -1,53 +1,22 @@
 import { Card, Form, Input, Checkbox, Button, message } from 'antd'
 import logo from '../../assets/logo.png'
-import { useNavigate } from 'react-router-dom'
 import './index.scss'
 
-import { useStore } from '../../store'
-
 const Login = () => {
-
-  const { loginStore } = useStore()
-  const navigate = useNavigate()
-  async function onFinish (values) {
-    console.log(values)
-    // values：放置的是所有表单项中用户输入的内容
-    // todo:登录
-    const { mobile, code } = values
-    await loginStore.getToken({ mobile, code })
-    // 跳转首页
-    navigate('/', { replace: true })
-    // 提示用户
-    message.success('登录成功')
-  }
-
   return (
     <div className="login">
       <Card className="login-container">
         <img className="login-logo" src={logo} alt="" />
-        {/* 登录表单 */}
-        {/* 子项用到的触发事件 需要在Form中都声明一下才可以 */}
-        <Form
-          validateTrigger={['onBlur', 'onChange']}
-          initialValues={{
-            remember: true,
-            mobile: '13811111111',
-            code: '246810'
-          }}
-          onFinish={onFinish}
-        >
+        <Form validateTrigger={['onBlur', 'onChange']}>
           <Form.Item
             name="mobile"
             rules={[
               {
-                required: true,
-                message: '请输入手机号',
-              },
-              {
                 pattern: /^1[3-9]\d{9}$/,
-                message: '请输入正确的手机号',
+                message: '手机号码格式不对',
                 validateTrigger: 'onBlur'
-              }
+              },
+              { required: true, message: '请输入手机号' }
             ]}
           >
             <Input size="large" placeholder="请输入手机号" />
@@ -55,24 +24,13 @@ const Login = () => {
           <Form.Item
             name="code"
             rules={[
-              {
-                required: true,
-                message: '请输入密码',
-              },
-              {
-                len: 6,
-                message: '请输入6位密码',
-                validateTrigger: 'onBlur'
-              }
+              { len: 6, message: '验证码6个字符', validateTrigger: 'onBlur' },
+              { required: true, message: '请输入验证码' }
             ]}
           >
-            <Input size="large" placeholder="请输入验证码" />
+            <Input size="large" placeholder="请输入验证码" maxLength={6} />
           </Form.Item>
-          <Form.Item
-            name="remember"
-            valuePropName="checked"
-
-          >
+          <Form.Item name="remember" valuePropName="checked">
             <Checkbox className="login-checkbox-label">
               我已阅读并同意「用户协议」和「隐私条款」
             </Checkbox>
