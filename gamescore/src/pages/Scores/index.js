ece471dc0c09286f3d556490ab72feb859a78fe1
Feng-Tianyu <80301864+Feng-Tianyu@users.noreply.github.com>
2023-06-14 15:33:54 +0800

    完成了评分页的跳转和参数传递

--- gamescore/src/pages/Scores/index.js
+++ gamescore/src/pages/Scores/index.js
@@ -1,72 +1,77 @@
 import { Card, Avatar, Menu, Select, Space, Input } from "antd";
 import "./index.scss";
+import { useNavigate } from 'react-router-dom'
 const { Search } = Input;
 const { Meta } = Card;
 
-// let gamelist = [
-//   {Input
-//     name: "Game1",
-//     score: 9.5,
-//     gameimg:
-//       "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
-//     platformimg: "https://xsgames.co/randomusers/avatar.php?g=pixel",
-//     date: "xxxx-xx-xx",
-//   },
-// ];
+let gamelist = [
+  {
+    name: "Game1",
+    info: "这是静态数据中的第一款游戏，这是静态数据中的第一款游戏这是静态数据中的第一款游戏这是静态数据中的第一款游戏，这是静态数据中的第一款游戏。这是静态数据中的第一款游戏这是静态数据中的第一款游戏，这是静态数据中的第一款游戏，这是静态数据中的第一款游戏这是静。这是静态数据中的第一款游戏。",
+    score: 9.5,
+    gameimg:
+      "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+    platformimg: "https://xsgames.co/randomusers/avatar.php?g=pixel",
+    date: "xxxx-xx-xx",
+    gameurl: "https://www.baidu.com",
+  },
+  {
+    name: "Game2",
+    info: "这是静态数据中的第二款游戏，这是静态数据中的第一款游戏这是静态数据中的第一款游戏这是静态数据中的第一款游戏，这是静态数据中的第一款游戏。这是静态数据中的第一款游戏这是静态数据中的第一款游戏，这是静态数据中的第一款游戏，这是静态数据中的第一款游戏这是静。这是静态数据中的第一款游戏。",
+    score: 9.7,
+    gameimg:
+      "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+    platformimg: "https://xsgames.co/randomusers/avatar.php?g=pixel",
+    date: "xxxx-xx-xx",
+    gameurl: "https://www.baidu.com",
+  }
+];
 
 // ↑真正的gamelist，等爬取的数据
 // ↓虚假的gamelist
-let gamelist = [
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "11",
-  "12",
-  "13",
-  "14",
-  "15",
-];
-
-const items = [
-  {
-    label: "首页",
-    key: "index",
-  },
-  {
-    label: "评分",
-    key: "score",
-  },
-];
+// let gamelist = [
+//   "1",
+//   "2",
+//   "3",
+//   "4",
+//   "5",
+//   "6",
+//   "7",
+//   "8",
+//   "9",
+//   "10",
+//   "11",
+//   "12",
+//   "13",
+//   "14",
+//   "15",
+// ];
 
 function Gamescorelist(games) {
+  const navigate = useNavigate()
+
   const count = games.length;
   const divs = Array.from({ length: count }, (_, index) => (
     <div className="gamecard" key={index}>
       <div className="score">
-        <p className="scorevalue">4.5</p>
+        <p className="scorevalue">{ games[index].score }</p>
       </div>
       <Card
+        onClick={() => navigate('/game', { state: games[index]}) }
         style={{ width: 300 }}
         cover={
           <img
             alt="example"
-            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+            src={ games[index].gameimg }
           />
         }
       >
         <Meta
           avatar={
-            <Avatar src="https://www.pinclipart.com/picdir/middle/100-1003109_steam-clip-art.png" />
+            <Avatar src={ games[index].platformimg } />
           }
-          title={"GAME " + games[index]}
-          description="xxxx-xx-xx发售"
+          title={ games[index].name }
+          description={ games[index].date + "发售"}
         />
       </Card>
     </div>
@@ -74,7 +79,27 @@ function Gamescorelist(games) {
   return <div className="games">{divs}</div>;
 }
 
+
 const Scores = () => {
+  const navigate = useNavigate()
+  const goTolayout = () => {
+      navigate('/')
+  }
+  const items = [
+    {
+      label: (
+        <span onClick={goTolayout} target="_blank" rel="noopener noreferrer">
+          首页
+        </span>
+      ),
+      key: "index",
+    },
+    {
+      label: "评分",
+      key: "score",
+    },
+  ];
+
   return (
     <div>
       <Menu selectedKeys="score" mode="horizontal" items={items} theme="dark" />
